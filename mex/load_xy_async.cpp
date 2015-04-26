11cd323b258b8904961652c9be248b4dbdbe8389
pengsun <pengsun000@gmail.com>
2015-04-26 01:00:34 -0400

    mat file pre-fetching
    asynchronous IO by starting worker thread; C++11 needed

---
+++ mex/load_xy_async.cpp
@@ -0,0 +1,77 @@
+#include "mex.h"
+#include "mat.h"
+#include <thread>
+
+using namespace std;
+
+
+void read_X_Y (const char *fn,  mxArray** X, mxArray** Y) {
+  MATFile *h = matOpen(fn, "r");
+  *X = matGetVariable(h, "X"); // TODO: check 
+  *Y = matGetVariable(h, "Y");
+  matClose(h);
+}
+
+struct mat_loader {
+  thread worker;  
+  mxArray *X, *Y;
+
+  mat_loader () {
+    X = mxCreateDoubleMatrix(0,0,mxREAL);
+    Y = mxCreateDoubleMatrix(0,0,mxREAL);
+  }
+
+  void load (const char * fn) {
+    // wait until last loading finishes 
+    if (worker.joinable()) worker.join();
+
+    // begin a new thread to load the variables
+    thread t(read_X_Y,  fn, &(this->X), &(this->Y));
+
+    // return immediately
+    this->worker = move(t);
+  }
+
+  void pop_buf (mxArray* &X, mxArray* &Y) {
+    // wait until last loading finishes...
+    if (worker.joinable()) 
+      worker.join();
+
+    // pop them
+    X = this->X;
+    this->X = mxCreateDoubleMatrix(0,0,mxREAL);
+
+    Y = this->Y;
+    this->Y = mxCreateDoubleMatrix(0,0,mxREAL);
+  }
+
+};
+
+static mat_loader the_loader;
+
+// load_xy_async(fn_mat); loads the the mat file with name fn_mat in a separate thread and returns immediately
+// [X,Y] = load_xy_async(); loads the X, Y from buffer 
+void mexFunction(int no, mxArray       *vo[],
+                 int ni, mxArray const *vi[])
+{
+  // load_xy_async(fn_mat): load mat file and return immediately
+  if (ni==1) {
+    // get the file name 
+    int buflen = mxGetN(vi[0])*sizeof(mxChar)+1;
+    char *buf  = mxMalloc(buflen);
+    mxGetString(vi[0], buf, buflen); // TODO: check status
+
+    // begin loading and return 
+    the_loader.load(buf);
+    return;
+  }
+
+  // [X,Y] = load_xy_async():  loads the X, Y from buffer
+  if (ni==0) {
+    the_loader.pop_buf(vo[0], vo[1]);
+    return;
+  }
+
+  mexErrMsgTxt("Incorrect calling arguments\n.");
+  return;
+}
--- mex/make_all.m
+++ mex/make_all.m
@@ -10,10 +10,11 @@ switch str(1:3)
     opt{end+1} = 'COMPFLAGS=/openmp $COMPFLAGS';
     opt{end+1} = 'LINKFLAGS=/openmp $LINKFLAGS';
   otherwise
-    opt{end+1} = 'CFLAGS=\$CFLAGS -fopenmp';
-    opt{end+1} = 'LDFLAGS=\$LDFLAGS -fopenmp';
+    opt{end+1} = 'CXXFLAGS="\$CXXFLAGS -fopenmp -std=c++11"';
+    opt{end+1} = 'LDFLAGS="\$LDFLAGS -fopenmp -std=c++11"';
 end
 %% do it
+mex(opt{:}, 'load_xy_asycn.cpp');
 mex(opt{:}, 'get_x_3slices.cpp');
 mex(opt{:}, 'get_y_g27s2.cpp');
 mex(opt{:}, 'toy.cpp');
